63d268670dae5c2fa34614c6299c88c78a57c94c
isabellagomez <ilgomezt@ucsd.edu>
2020-11-14 11:26:15 -0800

    TRY 1. rational.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Isabella Gomez A15305555
+# ECE143 HW7
+
+from rational import Rational
+import operator
+
+class Homework7:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    my_Homework7 = Homework7()
+    r = Rational(3,4)
+
+    #print(Rational(10,3) * Rational(101,8) - Rational(11,8))
+    print(sorted([Rational(10,3), Rational(9,8), Rational(10,1), Rational(1,100)]))
+    #print(sorted([Rational(10,3), Rational(9,8)]))
+    #print(repr(r))
+    #print(-r)
+    #print(-Rational(12345,128191) + Rational(101,103) * 30/44)
+    print(Rational(10,1))
---
+++ rational.py
@@ -0,0 +1,69 @@
+# Isabella Gomez A15305555
+# ECE143 HW7
+
+from fractions import Fraction
+class Rational:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+        # check that x and y are ints
+        assert type(x) == int and type(y) == int
+
+    def __repr__(self):
+        divided = Fraction(self.x, self.y)
+        if divided._denominator == 1:
+            return '%d'%divided._numerator
+        return '%d/%d'%(divided._numerator, divided._denominator)
+
+    def __float__(self):
+        return float(self.x/self.y)
+
+    def __int__(self):
+        return int(self.x/self.y)
+
+    def __mul__(self, other):
+        if not isinstance(other, (int, float)):
+            simplified = Fraction(self.x * other.x, self.y * other.y)
+            return Rational(simplified._numerator, simplified._denominator)
+        multiplied = Fraction(self.x,self.y) * Fraction(other)
+        return Rational(Fraction(multiplied)._numerator, Fraction(multiplied)._denominator)
+
+    def __add__(self, other):
+        added = Fraction(self.x,self.y) + Fraction(other.x, other.y)
+        return Rational(Fraction(added)._numerator, Fraction(added)._denominator)
+
+    def __sub__(self, other):
+        subbed = Fraction(self.x,self.y) - Fraction(other.x, other.y)
+        return Rational(Fraction(subbed)._numerator, Fraction(subbed)._denominator)
+
+    def __truediv__(self, other):
+        if not isinstance(other, (int, float)):
+            divided = Fraction(other.x,other.y)/Fraction(self.x,self.y)
+            return Rational(Fraction(divided)._numerator, Fraction(divided)._denominator)
+        divided = Fraction(self.x,self.y) / Fraction(other)
+        return Rational(Fraction(divided)._numerator, Fraction(divided)._denominator)
+
+    def __rtruediv__(self, other):
+        if not isinstance(other, (int, float)):
+            divided = Fraction(other.x, other.y) / Fraction(self.x, self.y)
+            return Rational(Fraction(divided)._numerator, Fraction(divided)._denominator)
+        fraction1 = Fraction(self.x, self.y)
+        faction2 = Fraction(other)
+        divided = faction2 / fraction1
+        return Rational(Fraction(divided)._numerator, Fraction(divided)._denominator)
+
+    def __neg__(self):
+        return Rational(-self.x,self.y)
+
+    def __lt__(self, other):
+        fraction1 = Fraction(self.x,self.y)
+        fraction2 = Fraction(other.x,other.y)
+
+        if fraction2 < fraction1:
+            return Rational(other.x,other.y)
+        elif fraction1 < fraction2:
+            return Rational(self.x,self.y)
+        else: # if they are equal do not return anything
+            return NotImplemented
